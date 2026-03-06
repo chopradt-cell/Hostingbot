de32861967b5d4e70bc61a6725731689c1fe91b1
chopradt-cell <chopradt@gmail.com>
2026-03-06 08:39:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,7 @@
 import telebot
+from flask import Flask
+import threading
+
 TOKEN = "8649446675:AAEqsrsDRkD9Jn1x0FgdjMA8N72Oz8qsSmk"
 CHANNEL_ID = -1003655366573
 
@@ -6,7 +9,7 @@ bot = telebot.TeleBot("8649446675:AAEqsrsDRkD9Jn1x0FgdjMA8N72Oz8qsSmk")
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "🚀 Bot is Working!")
+    bot.reply_to(message, "🚀 Bot is Running Successfully!")
 
 @bot.message_handler(content_types=['document'])
 def handle_file(message):
@@ -15,8 +18,16 @@ def handle_file(message):
         message.chat.id,
         message.message_id
     )
-
     link = f"https://t.me/c/{str(CHANNEL_ID)[4:]}/{sent.message_id}"
     bot.reply_to(message, f"✅ File Stored!\n🔗 {link}")
 
-bot.infinity_polling()
+threading.Thread(target=lambda: bot.infinity_polling()).start()
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
